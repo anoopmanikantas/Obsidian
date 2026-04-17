c638dc3a6059c17af3cf71d8a1bc978b917e8cca
Anoop Manikanta <anoop.manikanta.21@gmail.com>
2026-04-17 11:05:33 +0530

    Initial Commit

---
+++ Obsidian/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Obsidian
+//
+//  Created by Anoop Subramani on 17/04/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Obsidian/ObsidianApp.swift
@@ -0,0 +1,17 @@
+//
+//  ObsidianApp.swift
+//  Obsidian
+//
+//  Created by Anoop Subramani on 17/04/26.
+//
+
+import SwiftUI
+
+@main
+struct ObsidianApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
